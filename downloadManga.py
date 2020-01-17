49bf54bd2405385e274a317a94948f8278ae8456
famarajr <58300009+famarajr@users.noreply.github.com>
2020-01-17 15:51:07 +0000

    Update downloadManga.py

--- downloadManga.py
+++ downloadManga.py
@@ -1,14 +1,14 @@
-import bs4
-import requests
-import os
-import sys
-import re
+import bs4    #parser HTML
+import requests		#rend l’intégration avec des webservices très facile
+import os    #utiliser les fonctionnalités dépendantes du système d'exploitation
+import sys	
+import re	#opérations sur les expressions rationnelles
 from io import BytesIO
 from PIL import Image
 
-def getImages(urlEpisode, curChap):
+def getImages(urlEpisode, currentChap):	#defintion de la fonction qui permet de recuperer l'image
 	while urlEpisode:		
-		data = requests.get(urlEpisode)
+		data = requests.get(urlEpisode)		#Recuperation de l'URL de l'episode
 		base = bs4.BeautifulSoup(data.text, 'html.parser')
 		try:
 			img = base.select_one("img[id='img']").get('src')
@@ -19,20 +19,20 @@ def getImages(urlEpisode, curChap):
 		except Exception as e:
 			print("Telechargement impossible")
 
-		nextUrl  = base.select_one("div[id='imgholder'] a").get('href')
-		if int(nextUrl.split("/")[2]) != curChap:
+		nextUrl  = base.select_one("div[id='imgholder'] a").get('href')	#URL de l'episode suivant 
+		if int(nextUrl.split("/")[2]) != currentChap:
 			break
 		urlEpisode =f"{baseUrl+nextUrl}"
 
-
-def downloadManga(nom, params):
-	sep = "".join(re.findall(r"[-,]", params))
-	params = re.split(r"[-,]",sys.argv[2])
-	params = list(map(int, params))
-	url = f"{baseUrl}/{nom}"
-	if len(params)==2:
-		if sep == '-':
-			debut, fin = params[0], params[1]+1
+		
+def downloadManga(nom, parametres):			#defintion de la fonction qui indique comment telecharger l'image
+	sep = "".join(re.findall(r"[-,]", parametres))
+	parametres = re.split(r"[-,]",sys.argv[2])
+	parametres = list(map(int, parametres))
+	url = f"{baseUrl}/{nom}"		#Nouveau URL de la page
+	if len(parametres)==2:
+		if sep == '-':			#Cas dans lequel nous avons -
+			debut, fin = parametres[0], parametres[1]+1
 			for i in range(debut, fin):
 				urlEpisode = f"{url}/{i}/1"
 				if not os.path.exists(f"Chapitre{i}"):
@@ -43,12 +43,12 @@ def downloadManga(nom, params):
 						sys.exit()
 
 				os.chdir(f"Chapitre{i}")
-				getImages(urlEpisode, i)
+				getImages(urlEpisode, i)  #Appel de lafonction getImage
 				os.chdir("..")
 				
 
-		elif sep == ',':
-			for i in params:
+		elif sep == ',':		#Cas dans lequel nous avons ,
+			for i in parametres:
 				urlEpisode = f"{url}/{i}/1"
 				if not os.path.exists(f"Chapitre{i}"):
 					try:
@@ -61,8 +61,8 @@ def downloadManga(nom, params):
 				getImages(urlEpisode, i)
 				os.chdir("..")
 
-	elif len(params) == 1:
-		chap = params[0]
+	elif len(parametres) == 1:		#Cas dans lequel nous avons un seul parametre 
+		chap = parametres[0]
 		if not os.path.exists(f"Chapitre{chap}"):
 			try:
 				os.mkdir(f"Chapitre{chap}")
@@ -74,7 +74,7 @@ def downloadManga(nom, params):
 		urlEpisode = f"{url}/{chap}/1"
 		getImages(urlEpisode, chap)
 	print("Download Completed")
-if __name__ == "__main__":
+if __name__ == "__main__": #Page d'acceuil
 	nom = sys.argv[1]
 	chapiters = sys.argv[2]
 	baseUrl = "http://www.mangapanda.com"
